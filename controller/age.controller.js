62bca79c4c0b9ac319c2191c9a070a3b3971acbf
Bhawesh-Kommunicate <120364122+Bhawesh-Kommunicate@users.noreply.github.com>
2023-01-12 14:21:07 +0530

    emptying in age of symptom arr

--- controller/age.controller.js
+++ controller/age.controller.js
@@ -1,9 +1,10 @@
 
 const {age} = require("../service/age.service")
-
+const storage = await require("../service/store.service")
 const StoreAge = async (req, res) => {
   console.log(req.body.userId);
     const response = await age(req.body.userId, req.body.age);
+    const EmptySymptomArr = await storage.store(undefined , req.query.userId)
     return res.status(200).json({
       message: "data stored Successfully",
       success: "true",
