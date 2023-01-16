80e56bee0129a82036e1c79174997695ef1f00a6
Bhawesh-Kommunicate <120364122+Bhawesh-Kommunicate@users.noreply.github.com>
2023-01-16 14:52:04 +0530

    Gender Also corrected

--- controller/sex.controller.js
+++ controller/sex.controller.js
@@ -1,13 +1,14 @@
 const{gender} = require("../service/Gender.service")
 
 const StoreSex = async (req, res) => {
-  if(req.body.sex != "female" || req.body.sex !== "male"){
+  if(req.body.sex != "female" && req.body.sex !== "male"){
+    console.log(req.body.sex)
     let Gender = "female";
     // console.log(req.body.userId)
     let userId = req.body.userId;
     let response = await gender(Gender, userId);
     // console.log(response);
-    console.log("----------------------------------------");
+    console.log("--------------------------------IN ! PART--------");
     return res.status(200).json({
       message: "successfully Stored the data",
       data: response,
@@ -15,7 +16,7 @@ const StoreSex = async (req, res) => {
     });
   }else{
              // console.log(req.body.userId)
-    let Gender = req.boby.sex
+    let Gender = req.body.sex
     let userId = req.body.userId;
     let response = await gender(Gender, userId);
     // console.log(response);
